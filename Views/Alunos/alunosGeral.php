d842c8da3cdab5efa976bb1edd3e2b75f153ebcd
geovanipaz <geovanipaz7@gmail.com>
2025-12-01 21:25:29 -0300

    começo de tabela de clientes

--- Views/Alunos/alunosGeral.php
+++ Views/Alunos/alunosGeral.php
@@ -3,7 +3,76 @@
 ?>
 
 
-
+<section class="topProducts">
+    <h3>Top Selling Products</h3>
+    <table>
+        <thead>
+            <tr>
+                <th>Product</th>
+                <th>Stock</th>
+                <th>Price</th>
+                <th>Sales</th>
+                <th>Earnings</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>
+                    <div>
+                        <img src="./assets/product1.jpg" alt="">
+                    </div>
+                    <p>Winter Jacket for females</p>
+                </td>
+                <td>932</td>
+                <td>$23.99</td>
+                <td>1,838</td>
+                <td>$802,00</td>
+                <td>+11.5%</td>
+            </tr>
+            <tr>
+                <td>
+                    <div>
+                        <img src="./assets/product1.jpg" alt="">
+                    </div>
+                    <p>Winter Jacket for females</p>
+                </td>
+                <td>932</td>
+                <td>$23.99</td>
+                <td>1,838</td>
+                <td>$802,00</td>
+                <td>+11.5%</td>
+            </tr>
+            <tr>
+                <td>
+                    <div>
+                        <img src="./assets/product1.jpg" alt="">
+                    </div>
+                    <p>Winter Jacket for females</p>
+                </td>
+                <td>932</td>
+                <td>$23.99</td>
+                <td>1,838</td>
+                <td>$802,00</td>
+                <td>+11.5%</td>
+            </tr>
+            <tr>
+                <td>
+                    <div>
+                        <img src="./assets/product1.jpg" alt="">
+                    </div>
+                    <p>Winter Jacket for females</p>
+                </td>
+                <td>932</td>
+                <td>$23.99</td>
+                <td>1,838</td>
+                <td>$802,00</td>
+                <td>+11.5%</td>
+            </tr>
+        </tbody>
+    </table>
+    <small>See more...</small>
+</section>
 
 
 
